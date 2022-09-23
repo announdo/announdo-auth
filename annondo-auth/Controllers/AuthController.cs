9607870ac31791b88f4b44e55b86ba92d075c9b3
Encodeous <encodeous7@gmail.com>
2022-09-23 12:59:24 -0400

    Upload files

---
+++ annondo-auth/Controllers/AuthController.cs
@@ -0,0 +1,21 @@
+﻿using Google.Cloud.Firestore;
+using Microsoft.AspNetCore.Mvc;
+
+namespace annondo_auth.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class AuthController : ControllerBase
+{
+    public AuthController(FirestoreDb db)
+    {
+        _db = db;
+    }
+
+    public FirestoreDb _db { get; }
+    [HttpPost("login")]
+    public IActionResult Login(string token)
+    {
+        
+    }
+}
