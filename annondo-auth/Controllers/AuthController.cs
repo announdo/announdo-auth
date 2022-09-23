38fb48035cc069fc069591e64a34f6a7fff840f2
Adam Chen <encodeous7@gmail.com>
2022-09-23 14:15:02 -0400

    Add JWT authentication

--- annondo-auth/Controllers/AuthController.cs
+++ annondo-auth/Controllers/AuthController.cs
@@ -1,4 +1,5 @@
-﻿using Google.Cloud.Firestore;
+﻿using FirebaseAdmin.Auth;
+using Google.Cloud.Firestore;
 using Microsoft.AspNetCore.Mvc;
 
 namespace annondo_auth.Controllers;
@@ -14,8 +15,28 @@ public class AuthController : ControllerBase
 
     public FirestoreDb _db { get; }
     [HttpPost("login")]
-    public IActionResult Login(string token)
+    public async Task<IActionResult> Login([FromBody] string token)
     {
-        
+        var tickets = _db.Collection("tickets")!;
+        var snapshot = await tickets.WhereEqualTo("phrase", token)
+            .WhereEqualTo("type", "auth")
+            .Limit(1)
+            .GetSnapshotAsync();
+        if (!snapshot.Any())
+        {
+            return Unauthorized();
+        }
+
+        var ticket = snapshot[0];
+        var expiry = ticket.GetValue<Timestamp>("expiry").ToProto();
+        var claims = new Dictionary<string, object>()
+        {
+            {
+                "expiry", expiry.Seconds
+            },
+        };
+        string authToken = await FirebaseAuth.DefaultInstance.CreateCustomTokenAsync($"tok-{Guid.NewGuid()}", claims);
+
+        return Ok(authToken);
     }
 }
--- annondo-auth/Program.cs
+++ annondo-auth/Program.cs
@@ -1,6 +1,27 @@
+using FirebaseAdmin;
+using Google.Apis.Auth.OAuth2;
 using Google.Cloud.Firestore;
 
+if (!File.Exists("service_account.json"))
+{
+    Console.WriteLine($"Please download the service_account.json file from GCP and store it in the current folder! Expected directory {Environment.CurrentDirectory}");
+    return;
+}
+
+Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", Path.Combine(Environment.CurrentDirectory, "service_account.json"), EnvironmentVariableTarget.Process);
+
+FirebaseApp.Create(new AppOptions()
+{
+    Credential = GoogleCredential.GetApplicationDefault(),
+});
+
 var builder = WebApplication.CreateBuilder(args);
+
+if (builder.Configuration["gcp-id"] is null or "")
+{
+    Console.WriteLine($"Please specify a \"gcp-id\" in appsettings.json.");
+    return;
+}
 
 // Add services to the container.
 
